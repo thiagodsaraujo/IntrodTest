b09978680a7cb61a1aefcf59140672255ab01219
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-11-16 19:00:02 -0300

    Step 04 - Exploring other assert methods

--- test/MyAssertTest.java
+++ test/MyAssertTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MyAssertTest {
+
+    List<String> todos = Arrays.asList("AWS","Azure","DevOps");
+
+   @Test
+   void testeContains(){
+
+       String palavra = "AWSx";
+       boolean test = todos.contains(palavra);
+
+//       assertEquals(true, test);
+       assertEquals(3, todos.size());
+       assertTrue(test, "Deu errado!");
+   }
 }
